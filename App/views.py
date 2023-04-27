8ef572db70f2274ba55381f36080633795e47952
Surajshahi2020 <suraj.171245@ncit.edu.np>
2023-04-27 10:18:52 +0545

    first commit

---
+++ App/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
+
+# Create your views here.
+def login(request):
+    return render(request, "login.html")
+
+
+@login_required
+def home(request):
+    return render(request, "home.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
